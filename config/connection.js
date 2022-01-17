69a57e067021e421d9b2e9dbba51873600785511
Ivy Do <ivy.do2011@gmail.com>
2022-01-17 18:48:01 -0500

    set up handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const { rootCertificates } = require('tls');
 
 require('dotenv').config();
 
@@ -7,11 +8,15 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER, 
+    process.env.DB_PW, 
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 }
 
 module.exports = sequelize;
